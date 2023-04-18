ce9fb6acbd754a87aaeca390c7a8fa62e5c31f7d
supremkhatri <128308240+supremkhatri@users.noreply.github.com>
2023-04-18 09:55:04 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let addtodo = document.querySelector('.addtodo');
 let output = document.querySelector('.output')
 let datepicker = document.querySelector('.date-picker')
 
+
 let todolists = [];
 
 const render = () => {
@@ -10,23 +11,23 @@ const render = () => {
     todolists.forEach(function (todos, index) {
         let li = document.createElement('li');
         let deletebutton = document.createElement("button");
-
+        
         li.innerHTML = `
         <span>${index+1}.  </span> <span>${todos.title} </span>
         <span style="color: gray">${todos.dueDate}</span>
-      `;
+        `;
         deletebutton.innerText = "Delete";
-
+        
         li.appendChild(deletebutton);
         output.appendChild(li);
-
+        
         li.style.display = "flex";
         li.style.justifyContent = "space-between";
         li.style.width = "100%";
         li.style.alignItems = "center";
         li.style.paddingRight = "10px";
         li.style.marginBottom = "4px";
-
+        
         deletebutton.style.width = "30vwpx";
         deletebutton.style.backgroundColor = "red";
         deletebutton.style.border = "none";
@@ -37,12 +38,12 @@ const render = () => {
         deletebutton.style.marginRight = "20px";
         deletebutton.style.marginBottom = "4px";
         deletebutton.style.marginTop = "4px";
-
+        
         deletebutton.addEventListener("click", function () {
             let index = todolists.indexOf(todos);
             todolists.splice(index, 1);
             localStorage.setItem('todolists', JSON.stringify(todolists));// remove the list item from the DOM
-           render(); 
+            render(); 
         });
     });
 }
@@ -51,6 +52,10 @@ let savedTodos = JSON.parse(localStorage.getItem('todolists'));
 if (savedTodos !== null) {
     todolists = savedTodos;
     render();
+}
+
+if (todolists.length === 0) {
+    output.innerHTML= "No To Do Saved";
 }
 
 addtodo.addEventListener('click', () => {
